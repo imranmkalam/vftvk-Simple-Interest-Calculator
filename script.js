ec3dcb4dba36fb42368a5361b43f267b650790ca
imranmkalam <81364738+imranmkalam@users.noreply.github.com>
2021-07-08 10:26:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    document.getElementById("principal").innerHTML= "If you deposit!!!" + principal +",";
+
+    p=document.getElementById("principal").value;
+    r=document.getElementById("rate").value;
+    n=document.getElementById("years").value;
+    result=document.getElementById("result");
+    result.innerHTML="You will receive an amount of " + (p*n*r/100) + ",";
+
 }
-        
+
+
