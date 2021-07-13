4f8ed9885a24c2b32853eac7a5e3d5d42b20cfbf
imranmkalam <81364738+imranmkalam@users.noreply.github.com>
2021-07-13 19:20:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
   var slider = document.getElementById("myRange");
   var output = document.getElementById("value");
-  var principal_p = document.getElementById("result");
-  
-  principal_p.classList.add("highlight");
 
   output.innerHTML = slider.value;
 
   slider.oninput = function() {
     output.innerHTML = this.value;
 
-  principal_p.classList.add("highlight");
   } 
 })
 
 function validate(){  
   var num = document.myform.num.value;  
-  if (isNaN(num) || num == 0 || num < 0) {  
+  if (isNaN(num) || num == 0 || num < 0 || num == null) {  
     alert("Please enter a valid entry")
     //document.getElementById("principal").innerHTML="Enter Numeric value only";  
     return false; }
@@ -41,10 +37,10 @@ function compute() {
  
   const newDate = new Date();
   newDate.setFullYear(newDate.getFullYear() + n);
-  let result = "If you deposit " + p + ", " + "<br>" ;
-  result += "at an interest rate of " + r + "%," + "<br>";
-  result += "you will receive an amount of " + getFutureValue(p, r, n).toFixed(2) + "," + "<br>";
-  result += "in the year " + (newDate.getFullYear());
+  let result = "If you deposit <span class='highlight'>" + p + "</span>, " + "<br>" ;
+  result += "at an interest rate of <span class='highlight'>" + r + "%</span>," + "<br>";
+  result += "you will receive an amount of <span class='highlight'>" + getFutureValue(p, r, n) + "</span>," + "<br>";
+  result += "in the year <span class='highlight'>" + (newDate.getFullYear()) + "</span>";
   showResult(result);
 
 }
