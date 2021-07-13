87a8bde6189edcd18f27291fad9952d7eb6ee60f
Ammar Haider <ammar.haider@bjss.com>
2021-07-13 18:38:01 +0100

    highlights computed values

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
   var slider = document.getElementById("myRange");
   var output = document.getElementById("value");
-  var principal_p = document.getElementById("result");
   
-  principal_p.classList.add("highlight");
 
   output.innerHTML = slider.value;
 
   slider.oninput = function() {
     output.innerHTML = this.value;
-
-  principal_p.classList.add("highlight");
   } 
 })
 
@@ -41,10 +37,10 @@ function compute() {
  
   const newDate = new Date();
   newDate.setFullYear(newDate.getFullYear() + n);
-  let result = "If you deposit " + p + ", " + "<br>" ;
-  result += "at an interest rate of " + r + "%," + "<br>";
-  result += "you will receive an amount of " + getFutureValue(p, r, n).toFixed(2) + "," + "<br>";
-  result += "in the year " + (newDate.getFullYear());
+  let result = "If you deposit <span class='highlight'>" + p + "</span>, " + "<br>" ;
+  result += "at an interest rate of <span class='highlight'>" + r + "%</span>," + "<br>";
+  result += "you will receive an amount of <span class='highlight'>" + getFutureValue(p, r, n).toFixed(2) + "</span>," + "<br>";
+  result += "in the year <span class='highlight'>" + (newDate.getFullYear()) + "</span>s";
   showResult(result);
 
 }
